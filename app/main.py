e37bc31225c2d7bd06e53b4da9f5c1e8c6308f4c
Ivan Berezyanskyi <gamaew.res@gmail.com>
2022-11-10 13:38:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,61 @@
-class Validator:
-    pass
+from abc import abstractmethod, ABC
 
 
-class Number:
-    pass
+class Validator(ABC):
+
+    def __set_name__(self, owner: object, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: object, owner: object) -> None:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: object, value: any) -> None:
+        setattr(instance, self.protected_name, self.validate(value))
+
+    @abstractmethod
+    def validate(self, value: any) -> None:
+        pass
 
 
-class OneOf:
-    pass
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: int) -> int:
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+        elif self.max_value < value or self.min_value > value:
+            raise ValueError(f"Quantity should not be less than "
+                             f"{self.min_value} and greater than "
+                             f"{self.max_value}.")
+        return value
+
+
+class OneOf(Validator):
+    def __init__(self, options: list) -> None:
+        self.options = options
+
+    def validate(self, value: str) -> str:
+        if value not in self.options:
+            raise ValueError(f"Expected {value} to be one of "
+                             f"('ketchup', 'mayo', 'burger').")
+        return value
 
 
 class BurgerRecipe:
-    pass
+    sauce = OneOf(["ketchup", "mayo", "burger"])
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 3)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+
+    def __init__(self, buns: int, cheese: int, tomatoes: int,
+                 cutlets: int, eggs: int, sauce: str) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
