ffa6a0f94fe0ed3dd47233a01cda431cbc60e971
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-11-21 14:28:37 +0200

    create class Burger reciept and class Validator

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 class Validator:
-    pass
+    
+    def __setname__(self, name_of_the_attribute: str) -> str:
+        return "_" + name_of_the_attribute
+
+    def __set__(self, name_of_the_attribute: str, value: int) -> None:
+       name_of_the_attribute = value 
+    
+    def __get__(name_of_the_attribute):
+        return name_of_the_attribute
+    
+
 
 
 class Number:
@@ -11,4 +21,17 @@ class OneOf:
 
 
 class BurgerRecipe:
-    pass
+    def __init__(self,
+                 buns: int,
+                 cheese: int,
+                 tomatoes: int,
+                 cutlets: int,
+                 eggs: int,
+                 sauce: int)
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
+
