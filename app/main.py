f7beb1c657dcff258ca4278c5f5239589a4d8c3a
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-09 18:20:14 +0200

    Correct flake8 errors

--- app/main.py
+++ app/main.py
@@ -6,17 +6,17 @@ from typing import Any
 class Validator(ABC):
     def __set_name__(self, obj: Any, name: str) -> None:
         self.public_name = name
-        self.protected_name = '_' + name
+        self.protected_name = "_" + name
 
     def __get__(self, obj: Any, obj_type: Any) -> int | str:
         return getattr(obj, self.protected_name)
 
-    def __set__(self, obj, value):
+    def __set__(self, obj: Any, value: Any) -> None:
         self.validate(value)
         setattr(obj, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value) -> None:
+    def validate(self, value: Any) -> None:
         pass
 
 
@@ -25,18 +25,20 @@ class Number(Validator):
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value) -> None:
+    def validate(self, value: int) -> None:
         if type(value) is not int:
             raise TypeError("Quantity should be integer.")
         if not (self.min_value <= value <= self.max_value):
-            raise ValueError(f"Quantity should not be less than {self.min_value} and greater than {self.max_value}")
+            raise ValueError(f"Quantity should not be less than"
+                             f" {self.min_value} and"
+                             f" greater than {self.max_value}")
 
 
 class OneOf(Validator):
     def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
