115225bf22c77fd6e0ee33bb23124ee4d0cef1ea
Bohdan2001007 <bogdan1991007@gmail.com>
2023-04-18 18:28:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,30 +1,36 @@
-from abc import ABC
-from typing import Any, Type, Union
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any
 
 
 class Validator(ABC):
-    def __set_name__(self, owner: Type[object], name: str) -> None:
-        self.protected_name = f"_{name}"
+    def __set_name__(self, owner: BurgerRecipe, name: int | str) -> None:
+        self.protected_name = "_" + name
 
-    def __get__(self, instance: Any, owner: Type[object]) -> None:
+    def __get__(self, instance: BurgerRecipe, owner: BurgerRecipe) -> None:
         return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: BurgerRecipe, value: int) -> None:
+        self.validate(value)
+        return setattr(instance, self.protected_name, value)
+
+    @abstractmethod
+    def validate(self, value: Any) -> None:
+        pass
 
 
 class Number(Validator):
-    def __init__(
-            self, min_value: Union[int, float],
-            max_value: Union[int, float]
-    ) -> None:
+    def __init__(self, min_value: int, max_value: int) -> None:
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value: Any) -> None:
+    def validate(self, value: int) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
         if value < self.min_value or value > self.max_value:
             raise ValueError(
-                f"Quantity should not be less than {self.min_value}"
-                f" and greater than {self.max_value}."
+                f"Quantity should not be less than {self.min_value} "
+                f"and greater than {self.max_value}."
             )
 
 
@@ -32,7 +38,7 @@ class OneOf(Validator):
     def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value: Any) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
