5978b16129fd9b19ad92f43879e814f61ebb8590
Veinmax <aturjust88@gmail.com>
2023-06-23 13:44:22 +0300

    Test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,68 @@
-class Validator:
-    pass
+from abc import ABC, abstractmethod
 
 
-class Number:
-    pass
+class Validator(ABC):
+    def __set_name__(self, owner, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance, owner) -> int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value) -> None:
+        setattr(instance, self.protected_name, value)
+
+    @abstractmethod
+    def validate(self, value) -> None:
+        pass
 
 
-class OneOf:
-    pass
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+        if value not in range(self.max_value, self.max_value + 1):
+            raise ValueError("Quantity should not be less than"
+                             " {self.min_value} and greater than "
+                             "{self.max_value}.")
+
+
+class OneOf(Validator):
+    def __init__(
+            self,
+            options: tuple = ('ketchup', 'mayo', 'burger')
+    ) -> None:
+
+        self.options = options
+
+    def validate(self, value) -> None:
+        if value not in self.options:
+            raise ValueError(f"Expected {value} to be one of {self.options}.")
 
 
 class BurgerRecipe:
-    pass
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 4)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+    sauce = OneOf()
+
+    def __init__(
+            self,
+            buns: int,
+            cheese: int,
+            tomatoes: int,
+            cutlets: int,
+            eggs: int,
+            sauce: str
+    ) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
