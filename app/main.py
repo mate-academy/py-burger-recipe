7db7266c11fd2b0aa50c58ccff2e1dcb220ab47f
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-10-12 12:55:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,47 +1,73 @@
+from typing import Any, Type
 from abc import ABC, abstractmethod
 
 
 class Validator(ABC):
-
-    def __set_name__(self, owner: type, name: str) -> None:
+    def __set_name__(
+            self,
+            owner:
+            Type,
+            name: str
+    ) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, instance: object, owner: object) -> None:
+    def __get__(
+            self,
+            instance: object,
+            owner: Type
+    ) -> str:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance: object, value: int | str) -> None:
+    def __set__(
+            self,
+            instance: object,
+            value: Type
+    ) -> None:
+        self.validate(value)
         setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value: int | str) -> None:
+    def validate(
+            self,
+            value: Any
+    ) -> None:
         pass
 
 
 class Number(Validator):
-
-    def __init__(self, min_value: int, max_value: int) -> None:
+    def __init__(
+            self,
+            min_value: int,
+            max_value: int
+    ) -> None:
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value: int) -> bool:
+    def validate(
+            self,
+            value: Any
+    ) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
-        if not (self.min_value <= value <= self.max_value):
-            raise ValueError(f"Quantity should not be less "
-                             f"than {self.min_value} and "
-                             f"greater than {self.max_value}.")
-        return True
+        if value < self.min_value or value > self.max_value:
+            raise ValueError(f"Quantity should not be less than "
+                             f"{self.min_value} and greater "
+                             f"than {self.max_value}.")
 
 
 class OneOf(Validator):
-
-    def __init__(self, options: tuple) -> None:
+    def __init__(
+            self,
+            options: Any
+    ) -> None:
         self.options = options
 
-    def validate(self, value: str) -> bool:
+    def validate(
+            self,
+            value: Any
+    ) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
-        return True
 
 
 class BurgerRecipe:
