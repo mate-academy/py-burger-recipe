3100ced6c2c73b5533204ed9a60d6021e6c8ebb9
Vlad <mr.darmstadtium@gmail.com>
2023-11-17 20:59:04 +0100

    wrote all code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,65 @@
-class Validator:
-    pass
+from abc import ABC, abstractmethod
 
 
-class Number:
-    pass
+class Validator(ABC):
+
+    def __set_name__(self, owner, name: str):
+        self._name = "_" + name
+
+    def __get__(self, instance, owner) -> int | str:
+        return getattr(instance, self._name)
+
+    def __set__(self, instance, value: int | str):
+        setattr(instance, self._name, value)
+
+    @abstractmethod
+    def validate(self, value: int | str):
+        pass
 
 
-class OneOf:
-    pass
+class Number(Validator):
+
+    def __init__(self, min_value: int, max_value: int):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+
+        if not self.min_value <= value <= self.max_value:
+            raise ValueError(
+                f"Quantity should not be less than {self.min_value} "
+                f"and greater than {self.max_value}."
+            )
+
+
+class OneOf(Validator):
+
+    def __init__(self, options: list[str]):
+        self.options = options
+
+    def validate(self, value: str):
+        if value not in self.options:
+            raise ValueError(
+                f"Expected {value} to be"
+                f" one of {self.options}."
+            )
 
 
 class BurgerRecipe:
-    pass
+
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 3)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+    sauce = OneOf(["ketchup", "mayo", "burger"])
+
+    def __init__(self, buns: int, cheese: int, tomatoes: int, cutlets: int, eggs: int, sauce: str):
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
