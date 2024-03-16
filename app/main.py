a7fceefb19b51de992753791398b399989d7efe1
Mikola Rozhansky <bymikola@gmail.com>
2024-03-16 15:25:41 +0200

    Burger1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
 class Validator:
-    pass
-    pass2
-    pass3
-    pass4
 
 
 class Number:
@@ -14,4 +10,17 @@ class OneOf:
 
 
 class BurgerRecipe:
-    pass
+    def __init__(self,
+                 buns: int,
+                 cheese: int,
+                 tomatoes:int,
+                 cutlets: int,
+                 eggs: int,
+                 sauce: str) -> None:
+
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
