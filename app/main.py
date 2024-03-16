01514d6e71dc9a8df1b1efe599a1805bd5f74d01
Mikola Rozhansky <bymikola@gmail.com>
2024-03-16 15:42:21 +0200

    Number

--- app/main.py
+++ app/main.py
@@ -13,17 +13,23 @@ class Validator(ABC):
         return setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self):
+    def validate(self, value):
         pass
 
 
+class Number(Validator):
+    def __init__(self, min_value: int, max_value:int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f"Quantity should be integer.")
+
+        if not (self.min_value <= value <= self.max_value):
+            raise ValueError(f"Quantity should not be less than {self.min_value} and greater than {self.max_value}.")
 
 
-
-
-
-class Number:
-    pass
 
 
 class OneOf:
