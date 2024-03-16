b56e936c2485ddffde7178e9931b41dfe61706e9
Mikola Rozhansky <bymikola@gmail.com>
2024-03-16 17:44:51 +0200

    all_test_works

--- app/main.py
+++ app/main.py
@@ -3,38 +3,42 @@ from abc import ABC, abstractmethod
 
 class Validator(ABC):
 
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner: object, name: str) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: object, owner: object) -> None:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: object, value: int) -> None:
+        self.validate(value)
         return setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value):
+    def validate(self, value: int) -> None:
         pass
 
 
 class Number(Validator):
-    def __init__(self, min_value: int, max_value:int) -> None:
+    def __init__(self, min_value: int, max_value: int) -> None:
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value):
+    def validate(self, value: int) -> None:
         if not isinstance(value, int):
-            raise TypeError(f"Quantity should be integer.")
+            raise TypeError("Quantity should be integer.")
 
         if not (self.min_value <= value <= self.max_value):
-            raise ValueError(f"Quantity should not be less than {self.min_value} and greater than {self.max_value}.")
+            raise ValueError(f"Quantity should not be less "
+                             f"than {self.min_value} and greater "
+                             f"than {self.max_value}.")
 
 
 class OneOf(Validator):
-    def __init__(self, options):
+    def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value):
+    def validate(self, value: int) -> None:
+
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
@@ -45,12 +49,12 @@ class BurgerRecipe:
     tomatoes = Number(0, 3)
     cutlets = Number(1, 3)
     eggs = Number(0, 2)
-    sauce = OneOf(["ketchup", "mayo", "burger"])
+    sauce = OneOf(("ketchup", "mayo", "burger"))
 
     def __init__(self,
                  buns: int,
                  cheese: int,
-                 tomatoes:int,
+                 tomatoes: int,
                  cutlets: int,
                  eggs: int,
                  sauce: str) -> None:
